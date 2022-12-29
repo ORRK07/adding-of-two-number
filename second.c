dcaf4e3aff30d7bf0c8ff90b623bf52cfe8ffd09
Rahul Kumar <115874524+orrk2507@users.noreply.github.com>
2022-12-29 20:27:00 +0530

    Add files via upload

---
+++ second.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("enter the first number :");
+    scanf("%d",&a);
+    printf("enter second number :");
+    scanf("%d",&b);
+
+    printf("sum of two numbers : %d \n", a+b);
+    return 0;
+}
